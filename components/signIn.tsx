ca1106a5e4d26be6ed0452c1735b1fdd1d561976
小野 慶太 <dorokei@gmail.com>
2021-06-29 16:42:18 +0900

    Sign out

--- components/signIn.tsx
+++ components/signIn.tsx
@@ -6,19 +6,50 @@ import { AuthContext } from '../contexts/Auth';
 const SignIn: FC = () => {
   const { currentUser } = useContext(AuthContext);
 
-  useEffect(() => {
-    currentUser && Router.push('/')
-  }, [currentUser]);
+  // useEffect(() => {
+  //   currentUser && Router.push('/')
+  // }, [currentUser]);
 
   const login = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithRedirect(provider);
+    // firebase.auth().signInWithRedirect(provider);
+    firebase.auth().signInWithPopup(provider).then((result) => {
+      // const credential = result.credential;
+      // const token = credential?.providerId;
+      // const user = result.user;
+    }).catch((error) => {
+      // // Handle Errors here.
+      // const errorCode = error.code;
+      // const errorMessage = error.message;
+      // // The email of the user's account used.
+      // const email = error.email;
+      // // The firebase.auth.AuthCredential type that was used.
+      // const credential = error.credential;
+    });
   }
-  return (
-    <div className="container">
-      <button onClick={login}>googleでログインする</button>
-    </div>
-  )
+
+  const signout = () => {
+    firebase.auth().signOut().then(() => {
+      // logout
+    }).catch((error) => {
+      // error
+    });
+  }
+
+
+  if (!currentUser) {
+    return (
+      <div className="container">
+        <button onClick={login}>googleでログインする</button>
+      </div>
+    )
+  } else {
+    return (
+      <div className="container">
+        <button onClick={signout}>ログアウト</button>
+      </div >
+    )
+  }
 }
 
 export default SignIn;
