edb7566991f97c20726332a06daf7ca25e346173
小野 慶太 <dorokei@gmail.com>
2021-07-09 15:54:45 +0900

    Global header

--- components/CreateRoomButton.tsx
+++ components/CreateRoomButton.tsx
@@ -43,10 +43,15 @@ const CreateRoomButton: FC = () => {
   }
 
   return (
-    <div className="container">
-      <button onClick={createRoom}>休憩する</button>
+    <>
+      <button
+        className="border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+        onClick={createRoom}
+      >
+        休憩する
+      </button>
       {errorMessage}
-    </div>
+    </>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import { AuthProvider } from "../contexts/Auth";
+import MainLayout from "../components/MainLayout";
 
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
     </AuthProvider>
   );
 }
 
-export default MyApp;
+export default App;
